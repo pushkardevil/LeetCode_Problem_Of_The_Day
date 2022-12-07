e3dbc56c21165032df20f9df453a0bd81fc40174
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-07 10:04:33 +0530

    Update 06-12-22_Odd Even Linked List.cpp

--- 06-12-22_Odd Even Linked List.cpp
+++ 06-12-22_Odd Even Linked List.cpp
@@ -1,1 +1,26 @@
 
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if( head == NULL || head->next == NULL){
+            return head;
+        }
+        
+        ListNode* fast1=head;
+        ListNode*fast2=head->next;
+        ListNode* d=head->next;
+        
+
+        
+        while( fast2 != NULL && fast2->next != NULL ){
+            fast1->next=fast1->next->next;
+            fast2->next=fast2->next->next;
+            fast2=fast2->next;
+            fast1=fast1->next;
+        }
+        
+        fast1->next=d;
+        
+        return head;
+    }
+};
