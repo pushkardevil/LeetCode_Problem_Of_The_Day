1f2e93441d3fcdb6fec7b833c12be2f8d6e4464b
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2022-12-29 10:55:21 +0530

    Update 29-12-22_ Single-Threaded CPU.cpp

--- 29-12-22_ Single-Threaded CPU.cpp
+++ 29-12-22_ Single-Threaded CPU.cpp
@@ -1,28 +1,34 @@
 #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 class Solution {
 public:
-    vector<int> getOrder(vector<vector<int>>& A) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        int N = A.size();
-        long time = 0;
-        int i = 0;
-        for (int i = 0; i < N; ++i){
-            A[i].push_back(i);
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int>ans;
+        int n=tasks.size();
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>>pq;
+
+        for(int i=0;i<n;i++){
+            tasks[i].push_back(i);
         }
-        sort(begin(A), end(A));
-        vector<int> ans;
-        while (i < N || pq.size()){
-            if (pq.empty()) {
-                time = max(time, (long)A[i][0]);
+
+        sort(tasks.begin() , tasks.end());
+        long time=0;
+        int i=0;
+        while(i<n || pq.size()){
+            if(pq.empty() ){
+                time =max(time, (long)tasks[i][0]);
             }
-            while (i < N && time >= A[i][0]){
-                pq.push({A[i][1],A[i][2]});
-                ++i;
+
+            while(i < n && time >= tasks[i][0]){
+                pq.push({tasks[i][1] , tasks[i][2]});
+                i++;
             }
+
             pair<long,long> s= pq.top();
             pq.pop();
-            time += s.first;
+            time+=s.first;
             ans.push_back(s.second);
+
         }
         return ans;
 
