fe2d7a8b971e061b7b6b01196525386f6d7771a1
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-01-03 23:44:35 +0530

    Update 33-01-23_Detect Capital.cpp

--- 33-01-23_Detect Capital.cpp
+++ 33-01-23_Detect Capital.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
+        int n=word.size();
         if(isupper(word[0])){
             if(isupper(word[1])){
-                for(int i=2;i<word.length();i++){
+                for(int i=2;i<n;i++){
                     if(islower(word[i])){
                         return false;
                     }
                 }
-                
-            }else{
-                for(int i=1;i<word.length();i++){
+            }
+            else{
+                for(int i=1;i<n;i++){
                     if(isupper(word[i])){
                         return false;
                     }
                 }
             }
-            
-        }else{
-            for(int i=0;i<word.length();i++){
+
+        }
+        else{
+            for(int i=0;i<n;i++){
                 if(isupper(word[i])){
                     return false;
                 }
             }
+
         }
+
         return true;
     }
 };
