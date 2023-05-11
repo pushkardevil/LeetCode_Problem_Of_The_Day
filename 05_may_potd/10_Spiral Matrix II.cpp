f1ff473af992db2e973fd39fafa0e037808f3581
Pushkar Kumar <106862029+pushkardevil@users.noreply.github.com>
2023-05-11 13:42:42 +0530

    Update 10_Spiral Matrix II.cpp

--- 05_may_potd/10_Spiral Matrix II.cpp
+++ 05_may_potd/10_Spiral Matrix II.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>>ans(n , vector<int>(n,0));
 
-        int left=0,right=m-1;
-        int top=0, bottom=n-1;
-        vector<int>ans;
-
+        int left=0,right=n-1;
+        int top=0,bottom=n-1;
+        int j=1;
         while(top<=bottom && left <= right){
             for(int i=left;i<=right;i++){
-                ans.push_back(matrix[top][i]);
+                ans[top][i]=j;
+                j++;
             }
             top++;
             for(int i=top;i<=bottom;i++){
-                ans.push_back(matrix[i][right]);
+                ans[i][right]=j;
+                j++;
             }
             right--;
 
-            if(top <= bottom){
+            if(top<=bottom){
                 for(int i=right;i>=left;i--){
-                    ans.push_back(matrix[bottom][i]);
+                    ans[bottom][i]=j;
+                    j++;
                 }
-                bottom--;
             }
-            
-            if(left <= right){
+            bottom--;
+
+            if(left<= bottom){
                 for(int i=bottom;i>=top;i--){
-                    ans.push_back(matrix[i][left]);
+                    ans[i][left]=j;
+                    j++;
                 }
-                left++;
             }
-            
-            
+
+            left++;
         }
         return ans;
-        
     }
 };
